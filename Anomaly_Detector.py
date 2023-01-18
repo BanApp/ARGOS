977af2f35cc65b3f9bef3739b718638db674b047
MINJUN JUNG <93313445+BanApp@users.noreply.github.com>
2023-01-18 16:04:30 +0900

    Update Anomaly_Detector.py

--- Anomaly_Detector.py
+++ Anomaly_Detector.py
@@ -1,32 +1,77 @@
-from keras.models import load_model
+from pymongo import MongoClient
+from kafka import KafkaProducer
+import json
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
 import numpy as np
+from keras.models import load_model
 
-# Load the trained model
-autoencoder = load_model('autoencoder.h5')
+# Connect to MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['logs']
+collection = db['network_flows']
 
-# Preprocess new data in the same way as the training data
-scaler = MinMaxScaler()
-new_data = scaler.fit_transform(new_df)
+# Connect to Kafka
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
-# Use the model to predict the output for the new data
-predictions = autoencoder.predict(new_data)
+# Continuously read data from MongoDB and stream it to Kafka
+while True:
+    cursor = collection.find({}, {'_id': 0})
+    for document in cursor:
+        # Convert the document to a JSON string
+        data = json.loads(json.dumps(document))
+        # Extract the necessary information from the JSON object
+        IP = data["flow"]["IP"]["source"]
+        Port = data["flow"]["l4"]["UDP"]["destination_port"]
+        Pod_name = data["flow"]["source"]["pod_name"]
+        Namespace = data["flow"]["source"]["namespace"]
+        # Create a new dictionary with the extracted information
+        new_data = {"IP": IP, "Port": Port, "Pod_name": Pod_name, "Namespace": Namespace}
+        # Convert the new dictionary to a JSON string
+        data = json.dumps(new_data)
+        # Send the data to the "network_flows" topic in Kafka
+        producer.send('network_flows', value=data)
+        
 
-# Calculate the mean squared error between the predictions and the original data
-mse = np.mean(np.power(new_data - predictions, 2), axis=1)
+# Create a Spark Streaming context with a batch interval of 1 second
+sc = SparkContext("local[*]", "AnomalyDetection")
+ssc = StreamingContext(sc, 1)
 
-# Set a threshold for the mean squared error to define an anomaly
-threshold = 0.05
+# Create a Kafka stream using the KafkaUtils.createStream method
+kafkaStream = KafkaUtils.createStream(ssc, "localhost:2181", "anomaly_detection_group", {"network_flows": 1})
 
-# Find the data points that have a mean squared error greater than the threshold
-anomalies = np.where(mse > threshold)
+# Load the pre-trained autoencoder model
+autoencoder = load_model("autoencoder.h5")
 
-# Find the data points that have a mean squared error greater than the threshold
-anomalies = np.where(mse > threshold)
+# Define a function to preprocess the data and detect anomalies
+def detect_anomalies(rdd):
+    # Convert the RDD to a DataFrame
+    data = sqlContext.read.json(rdd)
 
-# Print the anomalous data points IP, port, pod name and namespace
-for index in anomalies[0]:
-    print("Anomalous data point:")
-    print("IP address:", new_df.iloc[index]["IP"]["source"])
-    print("Port:", new_df.iloc[index]["l4"]["UDP"]["source_port"])
-    print("Pod name:", new_df.iloc[index]["source"]["pod_name"])
-    print("Namespace:", new_df.iloc[index]["source"]["namespace"])
+    # Extract the necessary columns
+    data = data.select(["IP", "Port", "Pod_name", "Namespace", "flow.l4.UDP.destination_port"])
+
+    # Convert the DataFrame to a Numpy array
+    data = data.toPandas().values
+
+    # Preprocess the data
+    data = data.astype(float)
+    data = np.nan_to_num(data)
+
+    # Use the autoencoder to detect anomalies
+    predictions = autoencoder.predict(data)
+    mse = np.mean(np.power(data - predictions, 2), axis=1)
+
+    # Set a threshold for the mean squared error to define an anomaly
+    threshold = 3
+    anomalies = np.where(mse > threshold)
+    if len(anomalies[0]) > 0:
+        print("Anomaly detected!")
+
+    # Print the anomalous data points IP, port, pod name and namespace
+    for index in anomalies[0]:
+        print("Anomalous data point:")
+        print("IP address:", data.iloc[index]["IP"])
+        print("Port:", data.iloc[index]["Port"])
+       
