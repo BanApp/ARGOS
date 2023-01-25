88d3f82cda5cf1e53e0cb964a8d5fe9d37a4cb37
MINJUN JUNG <93313445+BanApp@users.noreply.github.com>
2023-01-25 12:01:43 +0900

    Update Trainer(Isolation Forest).py

--- AnomalyDetection/Trainer(Isolation Forest).py
+++ AnomalyDetection/Trainer(Isolation Forest).py
@@ -1,39 +1,114 @@
+from pymongo import MongoClient
+from pymongo.errors import PyMongoError
+import tensorflow as tf
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.ensemble import IsolationForest
 import joblib
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+import subprocess
+import time
+from datetime import datetime
 
-# 데이터 준비
-df = pd.read_csv("/content/drive/MyDrive/train.csv")
+warnings.filterwarnings("ignore", category=UserWarning, module="sklearn")
 
-features = ['IP_source', 'IP_destination', 'l4_TCP_source_port','l4_TCP_destination_port','l4_TCP_flags_ACK','source_identity','destination_identity','traffic_direction','event_type_type']
+# Connect to MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client["logs"]
+collection = db["network_flows"]
 
-df[features] = df[features].astype('str')
+features = ['IP_source', 'IP_destination', 'l4_TCP_source_port', 'l4_TCP_destination_port', 'l4_TCP_flags_ACK', 'source_identity', 'destination_identity',
+            'destination_labels_0','destination_labels_1','destination_labels_2','destination_labels_3','destination_labels_4',
+            'Type','traffic_direction','trace_observation_point''event_type_type','Summary']
 
-import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
+# 모델 호출
+model = joblib.load("model_if.joblib")
+encoder = joblib.load("encoder_if.joblib")
 
-# Create an instance of OneHotEncoder
-enc = OneHotEncoder(handle_unknown='ignore')
+# Set up change stream to listen for new insertions
+cnt = 1
+normal = 1
+anomaly = 0
+df1 = pd.DataFrame(columns=features)
+ck = 0
+while 1:
+    try:
+        change_stream = collection.watch()
+        # 인코더 호출
+        for change in change_stream: 
+            if change["operationType"] == "insert":
+                new_data = change["fullDocument"]
+                new_data = pd.DataFrame(new_data, index=[0])
+                new_data = new_data.reindex(columns=features, fill_value="nan")
+                new_data = new_data.astype('str')
+                if len(df1) < 10000:
+                    df1 = df1.append(new_data, ignore_index=True)
 
-# Apply one-hot encoding to specified columns
-one_hot_cols = pd.DataFrame(enc.fit_transform(df[['IP_source', 'IP_destination', 'l4_TCP_source_port','l4_TCP_destination_port','l4_TCP_flags_ACK','source_identity','destination_identity','traffic_direction','event_type_type']]).toarray())
+                elif len(df1) >= 10000:
+                    if ck == 0:
+                        normal = 1
+                        anomaly = 0
+                        ck = 1
 
-# Add one-hot encoded columns to the original dataframe
-df = pd.concat([df, one_hot_cols], axis=1)
+                    train_data = pd.DataFrame(encoder.fit_transform(df1[features]))
+                    model.fit(train_data)
 
-# Drop the original columns
-df.drop(['IP_source', 'IP_destination', 'l4_TCP_source_port','l4_TCP_destination_port','l4_TCP_flags_ACK','source_identity','destination_identity','traffic_direction','event_type_type'], axis=1, inplace=True)
+                    # Save the encoder
+                    #joblib.dump(encoder,"encoder_if.joblib")
+                    # Save the model
+                    #joblib.dump(model,"model_if.joblib")
 
+                    # 모델 호출
+                    #model = joblib.load("model_if.joblib")
+                    # 인코더 호출
+                    #encoder = joblib.load("encoder_if.joblib")
 
-# Create and train the model
-model = IsolationForest(random_state=1)
-model.fit(df)
+                    df1 = df1.iloc[2:]
+                    #print("normal: ",normal)
+                    #print("anomaly: ",anomaly)
+                    #print((normal/(anomaly+normal)*100))
+                    cnt = cnt + 1
 
+                    if anomaly+normal >= 1000:
+                        print("=======================DONE!=======================")
+                        print("AVR: ",(normal/(anomaly+normal)*100))
+                        print()
+                        normal = 1
+                        anomaly = 0
+                        cnt = 1
+                    #print()
 
-# Save the encoder
-joblib.dump(enc, "encoder_if.joblib")
+                # Encode the feature values with the appropriate labelEncoder
+                try:
+                    encoded_data = encoder.transform(new_data[features].values.reshape(-1, ).reshape(1, -1))
+                    data = pd.DataFrame(encoded_data, columns=encoder.get_feature_names_out(features))
 
-#Save the model
-joblib.dump(model, "model_if.joblib")
+                    # Use the predict method to detect outliers
+                    prediction = model.predict(data)
+
+                    # check the prediction, if it is -1, it means the new data is an outlier
+                    if prediction[0] == -1:
+                        anomaly = anomaly + 1
+                        #print("The new data is an outlier")
+                        #now = datetime.now()
+                        #print(now)
+                        #print()
+
+                    else:
+                        normal = normal + 1
+                        #collection.delete_one({'_id': new_data['_id']})
+                        #print("Good!")
+                        #now = datetime.now()
+                        #print(now)
+                        #print()
+                except:
+                    anomaly = anomaly + 1
+                    #print("The new data is an outlier")
+                    #now = datetime.now()
+                    #print(now)
+                    #print()
+
+    except PyMongoError as e:
+        print(e)
